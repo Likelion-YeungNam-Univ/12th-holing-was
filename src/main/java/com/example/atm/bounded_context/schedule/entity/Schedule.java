3e6b4506d1cff191dd8c9d7640eae9a7a53cc6ca
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-07-15 16:41:43 +0900

    feat: schedule entity 생성

---
+++ src/main/java/com/example/atm/bounded_context/schedule/entity/Schedule.java
@@ -0,0 +1,28 @@
+package com.example.atm.bounded_context.schedule.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = true)
+    private String place;
+
+    @Column(nullable = false)
+    private LocalDateTime startAt;
+
+    @Column(nullable = false)
+    private LocalDateTime finishAt;
+
+}
