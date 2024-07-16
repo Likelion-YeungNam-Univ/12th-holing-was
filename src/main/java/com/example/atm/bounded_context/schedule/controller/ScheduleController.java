6b5549779f8c5ce70d1a7d416d261b698e605f4e
Jsplix <jkjin112@gmail.com>
2024-07-17 02:57:03 +0900

    feat: schedule controller 구현

--- src/main/java/com/example/atm/bounded_context/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/atm/bounded_context/schedule/controller/ScheduleController.java
@@ -1,2 +1,33 @@
-package com.example.atm.bounded_context.schedule.controller;public class ScheduleController {
+package com.example.atm.bounded_context.schedule.controller;
+
+import com.example.atm.bounded_context.schedule.entity.Schedule;
+import com.example.atm.bounded_context.schedule.service.ScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/calendar")
+public class ScheduleController {
+
+    @Autowired
+    private ScheduleService scheduleService;
+
+    /**
+     * 특정 날짜의 일정을 조회
+     * @param date
+     * @return 해당 날짜의 모든 일정
+     */
+    @GetMapping
+    public ResponseEntity<List<Schedule>> getDateSchedule(@RequestParam("date") String date) {
+        LocalDate selectedDate = LocalDate.parse(date);
+        return new ResponseEntity<>(scheduleService.read(selectedDate), HttpStatus.OK);
+    }
 }
