8baae477fa493500d47a5b9d0043f29ac32a200f
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-07-16 17:49:08 +0900

    feat: auth kakao 메타 정보 config 작성

---
+++ src/main/java/com/example/atm/bounded_context/auth/config/KakaoProperties.java
@@ -0,0 +1,22 @@
+package com.example.atm.bounded_context.auth.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kakao")
+public class KakaoProperties {
+    private String authorizationUri;
+    private String tokenUri;
+    private String userInfoUri;
+    private String unlinkUri;
+    private String clientId;
+    private String clientSecret;
+    private String adminKey;
+    private String redirectUri;
+    private List<String> scope;
+}
