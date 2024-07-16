635f2778e2dca31d07f20e2de07147b9738f7744
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-07-17 02:16:13 +0900

    feat: user repository findByEmail 구현

---
+++ src/main/java/com/example/atm/bounded_context/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.atm.bounded_context.user.repository;
+
+import com.example.atm.bounded_context.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
