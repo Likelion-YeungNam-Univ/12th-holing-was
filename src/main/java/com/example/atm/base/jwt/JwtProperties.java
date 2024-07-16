f6e53431237ab8c0c18a4186c835ca0b92b3d9fb
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-07-17 02:11:14 +0900

    feat: jwt 메타 정보 config 구현

---
+++ src/main/java/com/example/atm/base/jwt/JwtProperties.java
@@ -0,0 +1,14 @@
+package com.example.atm.base.jwt;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+    private String issuer;
+    private String secretKey;
+    private Long expiration;
+}
