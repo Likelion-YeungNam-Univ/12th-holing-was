c3557a56ba30a38dbe8d64b34adae7cd8fddf364
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-07-16 18:13:27 +0900

    feat: auth service getToken 작성

---
+++ src/main/java/com/example/atm/bounded_context/auth/service/KakaoOAuthService.java
@@ -0,0 +1,65 @@
+package com.example.atm.bounded_context.auth.service;
+
+import com.example.atm.bounded_context.auth.config.KakaoProperties;
+import com.example.atm.bounded_context.auth.dto.TokenInfoDto;
+import com.example.atm.bounded_context.auth.dto.UserInfoDto;
+import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoOAuthService implements OAuthService{
+
+    private final KakaoProperties kakaoProperties;
+
+    /**
+     * 토큰 받기 : 인가 코드로 토큰 발급을 요청합니다.<br>
+     * https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#request-token
+     * @param code 인가 코드
+     * @return TokenInfoDto 발급된 토큰
+     * @throws HttpClientErrorException api 요청 실패 시 발생합니다.
+     */
+    @Override
+    public TokenInfoDto getToken(String code) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", kakaoProperties.getClientId());
+        params.add("client_secret", kakaoProperties.getClientSecret());
+        params.add("redirect_uri", kakaoProperties.getRedirectUri());
+        params.add("code", code);
+
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(kakaoProperties.getTokenUri(), entity, String.class);
+
+        JSONObject tokenInfoJson = new JSONObject(response.getBody());
+        String accessToken = tokenInfoJson.getString("access_token");
+        String refreshToken = tokenInfoJson.getString("refresh_token");
+        String scope = tokenInfoJson.getString("scope");
+
+        return TokenInfoDto.of(accessToken, refreshToken, scope);
+    }
+
+    @Override
+    public UserInfoDto getUserInfo(String accessToken) {
+        return null;
+    }
+
+    @Override
+    public void unlink(String socialId) {
+
+    }
+}
