fe31c82013fa7793d8364ef1b15b7df138facb0f
Jsplix <jkjin112@gmail.com>
2024-07-17 16:55:49 +0900

    feat: schedule repository 구현

--- src/main/java/com/example/atm/bounded_context/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/example/atm/bounded_context/schedule/repository/ScheduleRepository.java
@@ -4,13 +4,12 @@ import com.example.atm.bounded_context.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    Optional<List<Schedule>> findByDateBetween(LocalDate startDate, LocalDate finishDate);
+//    List<Schedule> findByStartAtOrderByStartAt(LocalDateTime dateTime);
 
-    List<Schedule> findByDateOrderByStartAt(LocalDate today);
+    List<Schedule> findByIdAndStartAtBetweenOrderByStartAtAsc(Long userId, LocalDateTime startAt, LocalDateTime finishAt);
 }
