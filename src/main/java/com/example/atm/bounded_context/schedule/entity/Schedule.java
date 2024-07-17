9fcab577505355b97d373aa07889d1443f71921d
Jsplix <jkjin112@gmail.com>
2024-07-17 16:55:16 +0900

    refact: update 메소드 및 user setter 추가

--- src/main/java/com/example/atm/bounded_context/schedule/entity/Schedule.java
+++ src/main/java/com/example/atm/bounded_context/schedule/entity/Schedule.java
@@ -1,12 +1,7 @@
 package com.example.atm.bounded_context.schedule.entity;
 
 import com.example.atm.bounded_context.user.entity.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,15 +15,20 @@ public class Schedule {
 
     @ManyToOne
     User user;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = true)
     private String place;
+
     @Column(nullable = false)
     private LocalDateTime startAt;
+
     @Column(nullable = false)
     private LocalDateTime finishAt;
 
