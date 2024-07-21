51e0ee255d996c8c8c6247008ba78bab23cfd8ae
Jsplix <jkjin112@gmail.com>
2024-07-21 17:38:43 +0900

    feat: mission response dto 생성

--- src/main/java/com/example/holing/bounded_context/mission/dto/MissionResponseDto.java
+++ src/main/java/com/example/holing/bounded_context/mission/dto/MissionResponseDto.java
@@ -1,2 +1,17 @@
-package com.example.holing.bounded_context.mission.dto;public record MissionResponseDto() {
+package com.example.holing.bounded_context.mission.dto;
+
+import com.example.holing.bounded_context.mission.entity.Mission;
+
+public record MissionResponseDto(
+        String content,
+        String tag,
+        int reward
+) {
+    public static MissionResponseDto fromEntity(Mission mission) {
+        return new MissionResponseDto(
+                mission.getContent(),
+                mission.getTag(),
+                mission.getReward()
+        );
+    }
 }
