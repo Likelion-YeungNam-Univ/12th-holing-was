9d59ace32c467576d058220564a170872c3926ec
Jsplix <jkjin112@gmail.com>
2024-07-21 17:42:34 +0900

    feat: mission result response dto 구현

--- src/main/java/com/example/holing/bounded_context/missionresult/repository/MissionResultRepository.java
+++ src/main/java/com/example/holing/bounded_context/missionresult/repository/MissionResultRepository.java
@@ -1,2 +1,13 @@
-package com.example.holing.bounded_context.missionresult.repository;public interface MissionResultRepository {
+package com.example.holing.bounded_context.missionresult.repository;
+
+import com.example.holing.bounded_context.missionresult.entity.MissionResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface MissionResultRepository extends JpaRepository<MissionResult, Long> {
+    List<MissionResult> findByCreateAtAndAndUserId(LocalDate createAt, Long userId);
 }
