601a332ffe34f8d8c826df391ecd648455ebb85e
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-07-24 03:53:02 +0900

    feat: survey entity 구현

---
+++ src/main/java/com/example/holing/bounded_context/survey/entity/Solution.java
@@ -0,0 +1,42 @@
+package com.example.holing.bounded_context.survey.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Solution {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private int minScore;
+
+    @Column(nullable = false)
+    private String summary;
+
+    @Column(nullable = false)
+    private String content1;
+
+    @Column(nullable = false)
+    private String content2;
+
+    @Column(nullable = false)
+    private String content3;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Tag tag;
+}
---
+++ src/main/java/com/example/holing/bounded_context/survey/entity/Tag.java
@@ -0,0 +1,30 @@
+package com.example.holing.bounded_context.survey.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "tag", fetch = FetchType.LAZY)
+    private List<Solution> solutions = new ArrayList<>();
+}
