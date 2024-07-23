15e7df983fb1b989d9ea6de69a603035d2db3da3
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-07-24 04:09:21 +0900

    feat: 증상 테스트 문항 조회 API 구현

---
+++ src/main/java/com/example/holing/bounded_context/survey/controller/QuestionController.java
@@ -0,0 +1,38 @@
+package com.example.holing.bounded_context.survey.controller;
+
+import com.example.holing.base.jwt.JwtProvider;
+import com.example.holing.bounded_context.survey.dto.QuestionResponseDto;
+import com.example.holing.bounded_context.survey.entity.Question;
+import com.example.holing.bounded_context.survey.service.QuestionService;
+import com.example.holing.bounded_context.user.entity.User;
+import com.example.holing.bounded_context.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/questions")
+public class QuestionController {
+    private final QuestionService questionService;
+    private final UserService userService;
+    private final JwtProvider jwtProvider;
+
+    @GetMapping("/symptom")
+    @Operation(summary = "증상 테스트 문항 조회", description = "사용자가 자가테스트의 문항을 조회하기 위한 API 입니다")
+    public ResponseEntity<List<QuestionResponseDto>> readQuestion(HttpServletRequest request) {
+        String accessToken = jwtProvider.getToken(request);
+        String userId = jwtProvider.getUserId(accessToken);
+
+        User user = userService.read(Long.parseLong(userId));
+        List<Question> questions = user.getIsPeriod() ? questionService.readAll() : questionService.readAllByTagNotPeriod();
+        List<QuestionResponseDto> response = questions.stream().map(QuestionResponseDto::fromEntity).toList();
+        return ResponseEntity.ok().body(response);
+    }
+}
