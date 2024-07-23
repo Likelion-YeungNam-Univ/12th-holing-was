ddaa912649dabdcf85e522ec5b5ea5cc21e065c8
Jsplix <jkjin112@gmail.com>
2024-07-24 01:39:48 +0900

    refact: Schedule API Swagger 추가에 따른 리팩토링

--- src/main/java/com/example/holing/bounded_context/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/holing/bounded_context/schedule/controller/ScheduleController.java
@@ -1,30 +1,27 @@
 package com.example.holing.bounded_context.schedule.controller;
 
 import com.example.holing.base.jwt.JwtProvider;
+import com.example.holing.bounded_context.schedule.api.ScheduleApi;
 import com.example.holing.bounded_context.schedule.dto.ScheduleRequestDto;
 import com.example.holing.bounded_context.schedule.dto.ScheduleResponseDto;
 import com.example.holing.bounded_context.schedule.service.ScheduleService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/calendar")
 @RequiredArgsConstructor
-public class ScheduleController {
+public class ScheduleController implements ScheduleApi {
 
     private final ScheduleService scheduleService;
 
@@ -36,17 +33,12 @@ public class ScheduleController {
      * @param date
      * @return 해당 날짜의 모든 일정
      */
-    @GetMapping("/schedules")
-    public ResponseEntity<?> getDateSchedule(HttpServletRequest request, @RequestParam("date") String date) {
-        try {
-            String accessToken = jwtProvider.getToken(request);
-            String userId = jwtProvider.getUserId(accessToken);
+    public ResponseEntity<List<ScheduleResponseDto>> getDateSchedule(HttpServletRequest request, @RequestParam("date") String date) {
+        String accessToken = jwtProvider.getToken(request);
+        String userId = jwtProvider.getUserId(accessToken);
 
-            LocalDate selectedDate = LocalDate.parse(date);
-            return new ResponseEntity<>(scheduleService.read(Long.parseLong(userId), selectedDate.atStartOfDay(), selectedDate.atStartOfDay().plusDays(1)), HttpStatus.OK);
-        } catch (DateTimeParseException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        LocalDate selectedDate = LocalDate.parse(date);
+        return ResponseEntity.ok().body(scheduleService.read(Long.parseLong(userId), selectedDate.atStartOfDay(), selectedDate.atStartOfDay().plusDays(1)));
     }
 
     /**
@@ -55,18 +47,13 @@ public class ScheduleController {
      * @param scheduleRequestDto
      * @return
      */
-    @PostMapping("/schedules")
-    public ResponseEntity<?> createSchedule(HttpServletRequest request, @RequestBody ScheduleRequestDto scheduleRequestDto) {
-        try {
-            String accessToken = jwtProvider.getToken(request);
-            String userId = jwtProvider.getUserId(accessToken);
+    public ResponseEntity<ScheduleResponseDto> createSchedule(HttpServletRequest request, @RequestBody ScheduleRequestDto scheduleRequestDto) {
+        String accessToken = jwtProvider.getToken(request);
+        String userId = jwtProvider.getUserId(accessToken);
 
-            ScheduleResponseDto scheduleResponseDto = scheduleService.create(Long.parseLong(userId), scheduleRequestDto);
+        ScheduleResponseDto scheduleResponseDto = scheduleService.create(Long.parseLong(userId), scheduleRequestDto);
 
-            return ResponseEntity.ok(scheduleResponseDto);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        return ResponseEntity.ok().body(scheduleResponseDto);
     }
 
     /**
@@ -75,16 +62,11 @@ public class ScheduleController {
      * @param scheduleId
      * @param scheduleRequestDto - 수정된 일정 사항
      */
-    @PutMapping("/schedules/{scheduleId}")
-    public ResponseEntity<?> updateSchedule(HttpServletRequest request, @PathVariable Long scheduleId, @RequestBody ScheduleRequestDto scheduleRequestDto) {
-        try {
-            String accessToken = jwtProvider.getToken(request);
-            String userId = jwtProvider.getUserId(accessToken);
+    public ResponseEntity<ScheduleResponseDto> updateSchedule(HttpServletRequest request, @PathVariable Long scheduleId, @RequestBody ScheduleRequestDto scheduleRequestDto) {
+        String accessToken = jwtProvider.getToken(request);
+        String userId = jwtProvider.getUserId(accessToken);
 
-            return ResponseEntity.ok(scheduleService.update(Long.parseLong(userId), scheduleId, scheduleRequestDto));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        return ResponseEntity.ok().body(scheduleService.update(Long.parseLong(userId), scheduleId, scheduleRequestDto));
     }
 
     /**
@@ -93,7 +75,7 @@ public class ScheduleController {
      * @param scheduleId
      */
     @DeleteMapping("/schedules/{scheduleId}")
-    public ResponseEntity<?> deleteSchedule(@PathVariable Long scheduleId) {
+    public ResponseEntity<String> deleteSchedule(@PathVariable Long scheduleId) {
         scheduleService.delete(scheduleId);
         return ResponseEntity.ok("일정 삭제 완료");
     }
