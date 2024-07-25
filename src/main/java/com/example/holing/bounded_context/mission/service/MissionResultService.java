e224651c8844a51d0c2a2b8d6445f7f5a8c6d2fe
Jsplix <jkjin112@gmail.com>
2024-07-25 21:38:37 +0900

    feat: 미션 랜덤 생성 시 태그 반영

--- src/main/java/com/example/holing/bounded_context/mission/service/MissionResultService.java
+++ src/main/java/com/example/holing/bounded_context/mission/service/MissionResultService.java
@@ -1,19 +1,19 @@
 package com.example.holing.bounded_context.mission.service;
 
-import com.example.holing.base.exception.GlobalException;
 import com.example.holing.bounded_context.mission.dto.MissionResultResponseDto;
 import com.example.holing.bounded_context.mission.entity.Mission;
 import com.example.holing.bounded_context.mission.entity.MissionResult;
 import com.example.holing.bounded_context.mission.repository.MissionRepository;
 import com.example.holing.bounded_context.mission.repository.MissionResultRepository;
 import com.example.holing.bounded_context.user.entity.User;
-import com.example.holing.bounded_context.user.exception.UserExceptionCode;
-import com.example.holing.bounded_context.user.repository.UserRepository;
+import com.example.holing.bounded_context.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -23,20 +23,19 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class MissionResultService {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final MissionResultRepository missionResultRepository;
     private final MissionService missionService;
     private final MissionRepository missionRepository;
 
     @Transactional
     public List<MissionResultResponseDto> create(Long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new GlobalException(UserExceptionCode.USER_NOT_FOUND));
+        User user = userService.read(userId);
 
-        List<Mission> dailyMission = missionService.getDailyMission(userId);
+        List<Mission> missions = missionService.getMissions(userId);
         List<MissionResult> todayMission = new ArrayList<>();
 
-        for (Mission mission : dailyMission) {
+        for (Mission mission : missions) {
             MissionResult missionResult = MissionResult.builder().build();
             missionResult.setMission(mission);
             missionResult.setUser(user);
@@ -52,10 +51,12 @@ public class MissionResultService {
 
     @Transactional(readOnly = true)
     public List<MissionResultResponseDto> read(Long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new GlobalException(UserExceptionCode.USER_NOT_FOUND));
+        User user = userService.read(userId);
 
-        List<MissionResult> todayMission = missionResultRepository.findAllByCreatedAtAndUserId(LocalDateTime.now(), userId);
+        LocalDateTime startOfDay = LocalDate.now().atStartOfDay();
+        LocalDateTime endOfDay = LocalDate.now().atTime(LocalTime.MAX);
+
+        List<MissionResult> todayMission = missionResultRepository.findAllByUserIdAndCreatedAtBetween(userId, startOfDay, endOfDay);
 
         return todayMission.stream()
                 .map(MissionResultResponseDto::fromEntity)
@@ -64,11 +65,13 @@ public class MissionResultService {
 
     @Transactional
     public MissionResultResponseDto update(Long userId, Long missionResultId) {
-
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new GlobalException(UserExceptionCode.USER_NOT_FOUND));
+        User user = userService.read(userId);
 
         MissionResult missionResult = missionResultRepository.findById(missionResultId).get();
+
+        if (missionResult.getCreatedAt() != missionResult.getModifiedAt()) {
+            throw new IllegalArgumentException("이미 변경된 미션입니다");
+        }
 
         // 완료된 미션에 대해서는 새로운 미션 받을 수 없음
         if (missionResult.isCompleted() == true) {
@@ -78,7 +81,6 @@ public class MissionResultService {
         // 동일한 태그(주요 증상)명의 미션 개수를 확인
         Mission prevMission = missionResult.getMission();
 
-        int count = missionRepository.countAllByTag(prevMission.getTag());
         List<Mission> newMissionList = missionRepository.findAllByTag(prevMission.getTag());
 
         // 난수 생성을 위한 Random 객체 선언
@@ -86,7 +88,7 @@ public class MissionResultService {
 
         // 난수 생성 후 미션 다시 받기 → 중복된 경우 한번더 진행
         while (true) {
-            int temp = random.nextInt(count);
+            int temp = random.nextInt(newMissionList.size());
             Mission tempMission = newMissionList.get(temp);
 
             // 임시로 발급한 미션이 기존의 생성된 미션이랑 일치하지 않는 경우 업데이트 후 break
--- src/main/java/com/example/holing/bounded_context/mission/service/MissionService.java
+++ src/main/java/com/example/holing/bounded_context/mission/service/MissionService.java
@@ -1,54 +1,69 @@
 package com.example.holing.bounded_context.mission.service;
 
-import com.example.holing.base.exception.GlobalException;
 import com.example.holing.bounded_context.mission.entity.Mission;
 import com.example.holing.bounded_context.mission.repository.MissionRepository;
+import com.example.holing.bounded_context.survey.entity.Tag;
+import com.example.holing.bounded_context.survey.repository.TagRepository;
 import com.example.holing.bounded_context.user.entity.User;
-import com.example.holing.bounded_context.user.exception.UserExceptionCode;
-import com.example.holing.bounded_context.user.repository.UserRepository;
+import com.example.holing.bounded_context.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MissionService {
 
     private final MissionRepository missionRepository;
-    private final UserRepository userRepository;
+    private final UserService userService;
+    private final UserReportService userReportService;
+    private final TagRepository tagRepository;
 
-    Set<Long> generateRandomNumbers() {
+    public List<Mission> generateRandomNumbers(List<Tag> tags) {
         Random random = new Random();
-        Set<Long> missionNumbers = new HashSet<>();
-        int totalMission = missionRepository.countAllBy();
+        tags.add(tagRepository.findById(7l).get());
+        List<Mission> tagMissions = missionRepository.findByTags(tags);
 
-        while (missionNumbers.size() < 3) {
-            Long number = random.nextLong(totalMission);
-            missionNumbers.add(number);
+        // 일상 태그의 미션은 따로 분리하여 랜덤 생성
+        List<Mission> dailyMissions = tagMissions.stream()
+                .filter(mission -> mission.getTag().getName().equals("DAILY"))
+                .collect(Collectors.toList());
+
+        Mission generatedDailyMission = dailyMissions.get(random.nextInt(dailyMissions.size()));
+
+        // 랜덤 생성 된 일상 미션 추가
+        List<Mission> randomMissions = new ArrayList<>();
+        randomMissions.add(generatedDailyMission);
+
+        // 증상 태그 중복 방지를 위해 Set 사용
+        Set<Tag> tagSet = new HashSet<>();
+        while (tagSet.size() < 2) {
+            int missionIndex = random.nextInt(tagMissions.size());
+            Mission tempMission = tagMissions.get(missionIndex);
+
+            //
+            if (tempMission.getTag().getName() == "DAILY") continue;
+
+            tagSet.add(tempMission.getTag());
+            randomMissions.add(tempMission);
         }
 
-        return missionNumbers;
+        return randomMissions;
     }
 
     @Transactional
-    public List<Mission> getDailyMission(Long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new GlobalException(UserExceptionCode.USER_NOT_FOUND));
+    public List<Mission> getMissions(Long userId) {
 
-        List<Mission> dailyMission = new ArrayList<>();
-        Set<Long> missionNumbers = generateRandomNumbers();
+        User user = userService.read(userId);
+        // 짝꿍의 증상 태그를 받아옴
+        List<Tag> mateTags = userReportService.getUserRecentReportTag(user.getMate());
 
-        for (Long id : missionNumbers) {
-            Mission mission = missionRepository.findById(id).get();
-            dailyMission.add(mission);
-        }
+        // 짝꿍의 증상 태그에 대한 미션 2개 + 일상 미션 1개를 받아옴
+        List<Mission> missions = generateRandomNumbers(mateTags);
 
-        return dailyMission;
+        return missions;
     }
 }
