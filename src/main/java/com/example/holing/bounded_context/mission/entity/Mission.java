d512bdf790bcf2ad5aad41ab5bbf0c6330e5cebc
Jsplix <jkjin112@gmail.com>
2024-07-25 21:23:09 +0900

    feat: tag 연관 관계 및 mission entity 필드 추가

--- src/main/java/com/example/holing/bounded_context/mission/entity/Mission.java
+++ src/main/java/com/example/holing/bounded_context/mission/entity/Mission.java
@@ -1,10 +1,7 @@
 package com.example.holing.bounded_context.mission.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.holing.bounded_context.survey.entity.Tag;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,18 +15,27 @@ public class Mission {
     private Long id;
 
     @Column(nullable = false)
-    private String content;
+    private String title;
 
     @Column(nullable = false)
-    private String tag;
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
 
     @Column(nullable = false)
     private int reward;
 
+    @Column(nullable = false)
+    private String description;
+
     @Builder
-    public Mission(String content, String tag, int reward) {
+    public Mission(String title, String content, Tag tag, int reward, String description) {
+        this.title = title;
         this.content = content;
         this.tag = tag;
         this.reward = reward;
+        this.description = description;
     }
 }
