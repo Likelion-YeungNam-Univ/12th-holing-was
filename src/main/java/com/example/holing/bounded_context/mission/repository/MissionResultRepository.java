e59133627c2ae26824b8322ca0305baefa991455
Jsplix <jkjin112@gmail.com>
2024-07-25 21:37:52 +0900

    refact: JPA 메소드 변경

--- src/main/java/com/example/holing/bounded_context/mission/repository/MissionResultRepository.java
+++ src/main/java/com/example/holing/bounded_context/mission/repository/MissionResultRepository.java
@@ -2,6 +2,8 @@ package com.example.holing.bounded_context.mission.repository;
 
 import com.example.holing.bounded_context.mission.entity.MissionResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -9,5 +11,10 @@ import java.util.List;
 
 @Repository
 public interface MissionResultRepository extends JpaRepository<MissionResult, Long> {
-    List<MissionResult> findAllByCreatedAtAndUserId(LocalDateTime createdAt, Long userId);
+    @Query("SELECT mr FROM MissionResult mr WHERE mr.user.id = :userId AND mr.createdAt BETWEEN :startOfDay AND :endOfDay")
+    List<MissionResult> findAllByUserIdAndCreatedAtBetween(
+            @Param("userId") Long userId,
+            @Param("startOfDay") LocalDateTime startOfDay,
+            @Param("endOfDay") LocalDateTime endOfDay
+    );
 }
