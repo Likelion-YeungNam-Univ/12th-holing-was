b5493942bf95ba02d4c483db6aecad98b1191a54
Jsplix <jkjin112@gmail.com>
2024-07-26 17:56:10 +0900

    refact: 순환참조에 의한 DTO 수정 및 Tag 필드 수정

---
+++ src/main/java/com/example/holing/bounded_context/mission/dto/MissionInfoDto.java
@@ -0,0 +1,22 @@
+package com.example.holing.bounded_context.mission.dto;
+
+import com.example.holing.bounded_context.mission.entity.Mission;
+
+public record MissionInfoDto(
+        Long id,
+        String missionTitle,
+        String missionContent,
+        String tagName,
+
+        int reward
+) {
+    public static MissionInfoDto FromEntity(Mission mission) {
+        return new MissionInfoDto(
+                mission.getId(),
+                mission.getTitle(),
+                mission.getContent(),
+                mission.getTag().getName(),
+                mission.getReward()
+        );
+    }
+}
--- src/main/java/com/example/holing/bounded_context/mission/dto/MissionResultResponseDto.java
+++ src/main/java/com/example/holing/bounded_context/mission/dto/MissionResultResponseDto.java
@@ -1,18 +1,17 @@
 package com.example.holing.bounded_context.mission.dto;
 
-import com.example.holing.bounded_context.mission.entity.Mission;
 import com.example.holing.bounded_context.mission.entity.MissionResult;
 
 public record MissionResultResponseDto(
         Long id,
         boolean isCompleted,
-        Mission mission
+        MissionInfoDto missionInfoDto
 ) {
     public static MissionResultResponseDto fromEntity(MissionResult missionResult) {
         return new MissionResultResponseDto(
                 missionResult.getId(),
                 missionResult.isCompleted(),
-                missionResult.getMission()
+                MissionInfoDto.FromEntity(missionResult.getMission())
         );
     }
 }
