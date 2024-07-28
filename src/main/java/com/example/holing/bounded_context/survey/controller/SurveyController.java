9aa781a2f6618ab548ed0e0bff0f75a320c782d6
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-07-29 01:52:24 +0900

    fix: survey 증상 및 자가테스트 문항 조회 end point 수정

--- src/main/java/com/example/holing/bounded_context/survey/controller/SurveyController.java
+++ src/main/java/com/example/holing/bounded_context/survey/controller/SurveyController.java
@@ -8,6 +8,7 @@ import com.example.holing.bounded_context.survey.service.SurveyService;
 import com.example.holing.bounded_context.user.entity.User;
 import com.example.holing.bounded_context.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +20,14 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/questions")
+@Tag(name = "[테스트 문항 관련 API]", description = "자가 / 증상 테스트 문항 조회 API")
+@RequestMapping("/survey")
 public class SurveyController {
     private final SurveyService surveyService;
     private final UserService userService;
     private final JwtProvider jwtProvider;
 
-    @GetMapping("/symptom")
+    @GetMapping("/symptom-test")
     @Operation(summary = "증상 테스트 문항 조회", description = "사용자가 증상 테스트의 문항을 조회하기 위한 API 입니다")
     public ResponseEntity<List<SymptomQuestionResponseDto>> readQuestion(HttpServletRequest request) {
         String accessToken = jwtProvider.getToken(request);
@@ -37,7 +39,7 @@ public class SurveyController {
         return ResponseEntity.ok().body(response);
     }
 
-    @GetMapping("/self")
+    @GetMapping("/self-test")
     @Operation(summary = "자가 테스트 문항 조회", description = "사용자가 자가 테스트의 문항을 조회하기 위한 API 입니다")
     public ResponseEntity<List<SelfQuestion>> readSelfQuestion(HttpServletRequest request) {
         String accessToken = jwtProvider.getToken(request);
