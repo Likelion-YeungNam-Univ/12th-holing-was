34eaf88423716bca0e97a68cdceb8cc5aec913a6
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-07-29 18:37:46 +0900

    fix: auth api 수정

--- src/main/java/com/example/holing/bounded_context/auth/api/AuthApi.java
+++ src/main/java/com/example/holing/bounded_context/auth/api/AuthApi.java
@@ -1,6 +1,5 @@
 package com.example.holing.bounded_context.auth.api;
 
-import com.example.holing.bounded_context.auth.dto.OAuthTokenInfoDto;
 import com.example.holing.bounded_context.auth.dto.SignInRequestDto;
 import com.example.holing.bounded_context.user.dto.UserInfoResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,23 +11,21 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
 
 @RequestMapping("/auth")
 @Tag(name = "[인증 관련 API]", description = "인증, 로그인 및 회원가입, 탈퇴 API")
 public interface AuthApi {
-    @GetMapping("/authorize")
-    @Operation(summary = "소셜 인증", description = "사용자가 소셜 인증을 받기 위한 API 입니다.<br>로그인 후 서비스 필수 항목에 동의가 완료되면 /auth/token 을 호출합니다.")
-    RedirectView authorize();
-
-    @GetMapping("/token")
-    @Operation(summary = "소셜 인증 토큰 반환", description = "사용자가 소셜 인증 토큰을 받기 위한 API 입니다.")
-    ResponseEntity<OAuthTokenInfoDto> token(@RequestParam("code") String code);
+//    @GetMapping("/authorize")
+//    @Operation(summary = "소셜 인증", description = "사용자가 소셜 인증을 받기 위한 API 입니다.<br>로그인 후 서비스 필수 항목에 동의가 완료되면 /auth/token 을 호출합니다.")
+//    RedirectView authorize();
+//
+//    @GetMapping("/token")
+//    @Operation(summary = "소셜 인증 토큰 반환", description = "사용자가 소셜 인증 토큰을 받기 위한 API 입니다.")
+//    ResponseEntity<OAuthTokenInfoDto> token(@RequestParam("code") String code);
 
     @PostMapping("/sign-in")
     @Operation(summary = "로그인 및 회원가입", description = "사용자가 로그인 및 회원가입을 위한 API 입니다.<br>회원가입의 경우 자가테스트의 사용자 정보가 필요합니다.")
