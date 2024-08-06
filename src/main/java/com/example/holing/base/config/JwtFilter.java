0c5f5c37b89a55b152342853975aeb2abd987002
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-08-06 19:54:09 +0900

    feat: filter 수정

--- src/main/java/com/example/holing/base/config/JwtFilter.java
+++ src/main/java/com/example/holing/base/config/JwtFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +20,21 @@ import java.io.IOException;
 public class JwtFilter extends OncePerRequestFilter {
     private final UserService userService;
     private final JwtProvider jwtProvider;
+    @Value("${webUrl}")
+    private String webUrl;
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return path.startsWith("/auth/") ||
+                path.startsWith("/survey/self-test") ||
+                path.startsWith("/swagger-resources/") ||
+                path.startsWith("/swagger-ui/") ||
+                path.startsWith("/v3/api-docs/") ||
+                path.startsWith("/webjars/") ||
+                path.startsWith("/error");
+    }
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -27,11 +43,11 @@ public class JwtFilter extends OncePerRequestFilter {
             String userId = jwtProvider.getUserId(accessToken);
             User user = userService.read(Long.parseLong(userId));
 
-            Authentication authentication = new UsernamePasswordAuthenticationToken(userId, null, null); //인증객체 생성
+            Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, null); //인증객체 생성
             SecurityContextHolder.getContext().setAuthentication(authentication); //인증정보 저장
             filterChain.doFilter(request, response);
         } catch (Exception e) {
-
+            response.sendRedirect("/auth/login");
         }
     }
 }
